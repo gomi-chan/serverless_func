1ba397ca5c20c453a82eee071c2d97165cc26470
gomi-chan <nakagomi@flourish-group.com>
2024-05-19 14:20:45 +0900

    testing deproy

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,6 +1,9 @@
 const items = require("../assets/data");
 exports.handler = async (event, context, cd) => {
   return {
+    Headers:{
+        'Access-Control-Allow-Origin':'*',
+    }
     statusCode: 200,
     body: JSON.stringify(items),
   };
