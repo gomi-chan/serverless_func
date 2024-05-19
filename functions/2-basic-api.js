14a31c3eb5d957de36580b8439ca4aa50ef15dab
gomi-chan <nakagomi@flourish-group.com>
2024-05-19 14:29:22 +0900

    修正

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,9 +1,7 @@
 const items = require("../assets/data");
 exports.handler = async (event, context, cd) => {
   return {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
+    Headers: { "Access-Control-Allow-Origin": "*" },
     statusCode: 200,
     body: JSON.stringify(items),
   };
